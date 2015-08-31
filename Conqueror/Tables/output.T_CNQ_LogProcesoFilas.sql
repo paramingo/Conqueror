2c6a04cdacc7241b4e2d952ae1e5b9af0f138fd2
inigo.mingo <inigo.mingo@thinksmart.es>
2015-09-01 01:45:25 +0200

    Tabla para registro de filas

---
+++ Conqueror/Tables/output.T_CNQ_LogProcesoFilas.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [output].[T_CNQ_LogProcesoFilas]
+(
+	[IdLinea] INT NOT NULL , 
+    [IdResultadoFila] INT  NULL, 
+    [IdLineaComparacion] INT NULL
+    PRIMARY KEY ([IdLinea], [IdResultadoFila]), 
+    CONSTRAINT [FK_T_CNQ_LogProcesoFilas_T_CNQ_ResultadosFilas] FOREIGN KEY ([IdResultadoFila]) REFERENCES [output].[T_CNQ_ResultadosFilas]([IdResultadoFila])
+)
