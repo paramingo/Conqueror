676d3e6fb37b14e537b72bd83d0545c7ea08c077
inigo.mingo <inigo.mingo@thinksmart.es>
2015-09-07 00:03:38 +0200

    Paquete y tabla de carga de los ficheros de Asociaciones

---
+++ Conqueror/Tables/input.T_CNQ_FicherosAsociaciones.sql
@@ -0,0 +1,43 @@
+﻿CREATE TABLE [input].[T_CNQ_FicherosAsociaciones] (
+    [IdFichero]             INT            NOT NULL,
+    [IdLinea]               INT            IDENTITY (1, 1) NOT NULL,
+    [Source]                NVARCHAR (80)  NULL,
+    [Continent]             NVARCHAR (20)  NULL,
+    [Country]               NVARCHAR (50)  NULL,
+    [City]                  NVARCHAR (100) NULL,
+    [Email]                 NVARCHAR (100) NULL,
+    [Title]                 NVARCHAR (5)   NULL,
+    [FirstName]             NVARCHAR (255) NULL,
+    [Surname]               NVARCHAR (255) NULL,
+    [JobTitle]              NVARCHAR (200) NULL,
+    [CompanyName]           NVARCHAR (255) NULL,
+    [Address]               NVARCHAR (500) NULL,
+    [PostalCode]            NVARCHAR (40)  NULL,
+    [TelephoneNo]           NVARCHAR (200) NULL,
+    [MobileNo]              NVARCHAR (200) NULL,
+    [Fax]                   NVARCHAR (200) NULL,
+    [Website]               NVARCHAR (200) NULL,
+    [COOPStatusLead]        NVARCHAR (60)  NULL,
+    [CQRStatusLead]         NVARCHAR (60)  NULL,
+    [COOPStatusCity]        NVARCHAR (40)  NULL,
+    [CQRStatusCity]         NVARCHAR (40)  NULL,
+    PRIMARY KEY CLUSTERED ([IdFichero] ASC, [IdLinea] ASC),
+    CONSTRAINT [FK_T_CNQ_FicherosAsociaciones_T_CNQ_FicherosRegistro] FOREIGN KEY ([IdFichero]) REFERENCES [input].[T_CNQ_FicherosRegistro] ([IdFichero])
+);
+
+
+GO
+
+GRANT INSERT
+    ON OBJECT::[input].[T_CNQ_FicherosAsociaciones] TO [CNQ_SSISUser]
+    AS [dbo];
+
+
+GO
+
+GRANT SELECT
+    ON OBJECT::[input].[T_CNQ_FicherosAsociaciones] TO [CNQ_SSISUser]
+    AS [dbo];
+
+
+GO
