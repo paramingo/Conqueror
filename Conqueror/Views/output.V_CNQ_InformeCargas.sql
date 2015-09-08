379cf1f7e7e23890af27fa0ca1c41d230533b334
inigo.mingo <inigo.mingo@thinksmart.es>
2015-09-08 16:58:43 +0200

    Vistas de información para el Excel

---
+++ Conqueror/Views/output.V_CNQ_InformeCargas.sql
@@ -0,0 +1,19 @@
+﻿CREATE VIEW [output].[V_CNQ_InformeCargas]
+AS
+SELECT R.IdFichero, IdFicheroTipo, DsFichero, FcInicioCarga, FcFinCarga
+	,CASE WHEN COUNT(DISTINCT F.IDLINEA) > 0 THEN COUNT(DISTINCT F.IDLINEA) ELSE COUNT(DISTINCT A.IDLINEA) END AS NumeroFilasCargadas
+FROM [input].[T_CNQ_FicherosRegistro] R
+LEFT OUTER JOIN [input].[T_CNQ_Ficheros] F ON R.IdFichero = F.IdFichero
+LEFT OUTER JOIN [input].[T_CNQ_FicherosAsociaciones] A ON R.IdFichero = A.IdFichero
+WHERE R.FcFinCarga IS NOT NULL
+GROUP BY R.IdFichero, IdFicheroTipo, DsFichero, FcInicioCarga, FcFinCarga
+
+GO
+
+GRANT SELECT
+    ON OBJECT::[output].[V_CNQ_InformeCargas] TO [CNQ_Conqueror]
+    AS [dbo];
+
+
+GO
+
---
+++ Conqueror/Views/output.V_CNQ_InformeTrazaContactos.sql
@@ -0,0 +1,31 @@
+﻿CREATE VIEW [output].[V_CNQ_InformeTrazaContactos]
+AS
+SELECT IdContacto
+	,DsFichero, Source, Continent, Country, City, Email, Title, FirstName, Surname, JobTitle
+	,CompanyName, Address, PostalCode, TelephoneNo, MobileNo, Fax, Website
+	,COOPStatusLead, CQRStatusLead, COOPStatusCity, CQRStatusCity
+FROM (
+	SELECT IdContactoComparacion AS IdContacto
+		,LPF.IdFichero, LPF.IdLinea, Source, Continent, Country, City, Email, Title, FirstName, Surname, JobTitle
+		,CompanyName, Address, PostalCode, TelephoneNo, MobileNo, Fax, Website
+		,COOPStatusLead, CQRStatusLead, COOPStatusCity, CQRStatusCity
+	FROM [output].[T_CNQ_LogProcesoFilas] LPF
+	INNER JOIN [input].[T_CNQ_FicherosAsociaciones] A ON LPF.IdLinea = A.IdLinea AND LPF.IdFichero = A.IdFichero
+	UNION ALL
+	SELECT IdContactoComparacion AS IdContacto
+		,LPF.IdFichero, LPF.IdLinea, Source, Continent, Country, City, Email, NULL, TitleFirstNameSurname, NULL, NULL
+		,CompanyName, Address, PostalCode, TelephoneNo, NULL, NULL, NULL
+		,COOPStatusLead, CQRStatusLead, COOPStatusCity, CQRStatusCity
+	FROM [output].[T_CNQ_LogProcesoFilas] LPF
+	INNER JOIN [input].[T_CNQ_Ficheros] F ON LPF.IdLinea = F.IdLinea AND LPF.IdFichero = F.IdFichero
+) Lineas
+INNER JOIN [input].[T_CNQ_FicherosRegistro] FR ON FR.IdFichero =  Lineas.IdFichero
+
+GO
+
+GRANT SELECT
+    ON OBJECT::[output].[V_CNQ_InformeTrazaContactos] TO [CNQ_Conqueror]
+    AS [dbo];
+
+
+GO
